5fd4a12fc8b7ed22b09138945ca9907293c6587d
NilsSluiter <52400711+NilsSluiter@users.noreply.github.com>
2019-07-01 19:04:23 +0200

    Add files via upload

--- test_tijdfuncties.py
+++ test_tijdfuncties.py
@@ -13,23 +13,23 @@ class test_lijstnaartekst(unittest.TestCase):
     def test_zin_lege_list(self):
         self.assertEqual(tijdfuncties.lijstnaartekst([""]), "")
 
-# test dateobjectnaartekst
+# test datumobjectnaartekst
     def test_datumobject(self):
-        self.assertEqual(tijdfuncties.dateobjectnaartekst(datetime.datetime(2019, 7, 1)), "maandag 1 juli")
+        self.assertEqual(tijdfuncties.datumobjectnaartekst(datetime.datetime(2019, 7, 1)), "maandag 1 juli")
     def test_datumtekst(self):
-        self.assertEqual(tijdfuncties.dateobjectnaartekst(datetime.datetime(2019, 7, 1, 12, 30)), "maandag 1 juli")
+        self.assertEqual(tijdfuncties.datumobjectnaartekst(datetime.datetime(2019, 7, 1, 12, 30)), "maandag 1 juli")
 
-# test dateobjectnaarstring
+# test datumobjectnaarstring
     def test_object(self):
-        self.assertEqual(tijdfuncties.dateobjectnaarstring(datetime.datetime(2019, 7, 1)), "2019-07-01")
+        self.assertEqual(tijdfuncties.datumobjectnaarstring(datetime.datetime(2019, 7, 1)), "2019-07-01")
     def test_object_tijd(self):
-        self.assertEqual(tijdfuncties.dateobjectnaarstring(datetime.datetime(2019, 7, 1, 12, 30), True), "2019-07-01, 12:30:00")
+        self.assertEqual(tijdfuncties.datumobjectnaarstring(datetime.datetime(2019, 7, 1, 12, 30), True), "2019-07-01, 12:30:00")
 
-# test stringnaardateobject
+# test stringnaardatumobject
     def test_string(self):
-         self.assertEqual(tijdfuncties.stringnaardateobject("2017-12-31"), datetime.datetime(2017, 12, 31, 0, 0))
+         self.assertEqual(tijdfuncties.stringnaardatumobject("2017-12-31"), datetime.datetime(2017, 12, 31, 0, 0))
     def test_foute_datum(self):
-         self.assertEqual(tijdfuncties.stringnaardateobject("20-17-123-1"), None)
+         self.assertEqual(tijdfuncties.stringnaardatumobject("20-17-123-1"), None)
         
 if __name__ == '__main__':
     unittest.main()
--- tijdfuncties.py
+++ tijdfuncties.py
@@ -4,10 +4,10 @@ maanden = ["januari","februari","maart","april","mei","juni","juli","augustus","
 dagen = ["zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag"]
 
 # datetime-object naar leesbare tekst
-def dateobjectnaartekst(dtm):
-    maand = maanden[int(dtm.strftime("%m")) -1] #-1 omdat een list bij 0 begint en maanden bij 1
-    dag = int(dtm.strftime("%d"))
-    weekdag = dagen[int(dtm.strftime("%w"))]
+def datumobjectnaartekst(datumobj):
+    maand = maanden[int(datumobj.strftime("%m")) -1] #-1 omdat een list bij 0 begint en maanden bij 1
+    dag = int(datumobj.strftime("%d"))
+    weekdag = dagen[int(datumobj.strftime("%w"))]
     return "{} {} {}".format(weekdag, dag, maand)
 
 # lijst naar leesbare tekst
@@ -17,16 +17,16 @@ def lijstnaartekst(woorden):
     return "{} en {}".format(", ".join(woorden[:-1]),  woorden[-1])
     
 # datetime object naar string
-def dateobjectnaarstring(dtobject, met_tijd = False):
+def datumobjectnaarstring(datumobj, met_tijd = False):
     if met_tijd:
-        date_time = dtobject.strftime("%Y-%m-%d, %H:%M:%S")
+        date_time = datumobj.strftime("%Y-%m-%d, %H:%M:%S")
     else:
-        date_time = dtobject.strftime("%Y-%m-%d")
+        date_time = datumobj.strftime("%Y-%m-%d")
     return date_time
 
 # zet string om in datetime-object 
-def stringnaardateobject(dtm):
+def stringnaardatumobject(datumobj):
     try:
-        return datetime.datetime.strptime(dtm, '%Y-%m-%d')
+        return datetime.datetime.strptime(datumobj, '%Y-%m-%d')
     except:
         return None
